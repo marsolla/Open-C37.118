a4da8d10be5926d9b46b58ebca87088faec48702
Fat <fatdestruction@gmail.com>
2021-10-08 22:26:11 -0300

    added GPL headers

--- examples/pmu.c
+++ examples/pmu.c
@@ -1,4 +1,4 @@
-/**(C) Copyright 2013 Rafael Marsolla
+/**(C) Copyright 2014 Rafael Marsolla - rafamarsolla@gmail.com
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
